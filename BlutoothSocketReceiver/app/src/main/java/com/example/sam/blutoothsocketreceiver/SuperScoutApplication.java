5e3bd1bacb2abf6926db9f7b3b67c4046ae4c5de
Sam Chung <meetsammy1007@gmail.com>
2016-04-17 09:56:20 -0700

    Super now saves error logs for UI thread crash and background thread crash

--- BlutoothSocketReceiver/app/src/main/java/com/example/sam/blutoothsocketreceiver/SuperScoutApplication.java
+++ BlutoothSocketReceiver/app/src/main/java/com/example/sam/blutoothsocketreceiver/SuperScoutApplication.java
@@ -24,7 +24,10 @@ import com.instabug.library.Instabug;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -87,77 +90,79 @@ public class SuperScoutApplication extends Application implements Application.Ac
         });
 
     }
-    public void onActivityCreated(Activity activity, Bundle savedInstanceState){
-        currentActivity = activity;
-    }
-    public void onActivityDestroyed(Activity activity){
-        currentActivity = null;
-    }
-    public void onActivityPaused(Activity activity){
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState){currentActivity = activity;}
 
-    }
-    public void onActivityResumed(Activity activity){
+    public void onActivityDestroyed(Activity activity){currentActivity = null;}
 
-    }
-    public void onActivitySaveInstanceState(Activity activity, Bundle outState){
+    public void onActivityPaused(Activity activity){}
 
-    }
-    public void onActivityStarted(Activity activity){
+    public void onActivityResumed(Activity activity){}
 
-    }
-    public void onActivityStopped(Activity activity){
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState){}
 
-    }
+    public void onActivityStarted(Activity activity){}
+
+    public void onActivityStopped(Activity activity){}
 
     private void handleUncaughtException (Thread thread, Throwable e)
     {
-        List<Class<? extends ActionBarActivity>> activities = Arrays.asList(MainActivity.class, FinalDataPoints.class);
         // The following shows what I'd like, though it won't work like this.
         if (Looper.myLooper() == Looper.getMainLooper()) {
+            Writer result = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(result);
+            e.printStackTrace(printWriter);
+            String stacktrace = result.toString();
+            saveErrorLog("/sdcard/Super_UIError_log", stacktrace);
+            printWriter.close();
             Log.e("UI thread", "CRASHED!");
-            Log.e("Activity crashed", currentActivity.toString());
             takeScreenshot();
             originalUncaughtExceptionHandler.uncaughtException(thread, e);
         }else{
+            Writer result = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(result);
+            e.printStackTrace(printWriter);
+            String stacktrace = result.toString();
+            saveErrorLog("/sdcard/Super_threadError_log", stacktrace);
             Log.e("Background thread", "CRASHED");
             Toast.makeText(this, "Background Thread ERROR", Toast.LENGTH_LONG).show();
         }
 
     }
     private void takeScreenshot() {
-        Date now = new Date();
-        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
         File dir = new File("/sdcard/Super_scout_backup");
         Log.e("dir", dir.getAbsolutePath());
         dir.mkdir();
         File photoFile = new File(dir, (new SimpleDateFormat("MM-dd-yyyy-H:mm:ss").format(new Date())) + ".jpg");
         try {
             // image naming and path  to include sd card  appending name you choose for file
-            Log.e("test", "1");
             FileOutputStream file = new FileOutputStream(photoFile);
-            // create bitmap screen capture
-            Log.e("Test", "2");
             View v1 = currentActivity.getWindow().getDecorView().getRootView();
-            Log.e("Test", "3");
             v1.setDrawingCacheEnabled(true);
-            Log.e("Test", "4");
             Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
-            Log.e("Test", "5");
             v1.setDrawingCacheEnabled(false);
-            Log.e("Test", "6");
             int quality = 100;
             bitmap.compress(Bitmap.CompressFormat.JPEG, quality, file);
-            Log.e("Test", "7");
             file.flush();
-            Log.e("Test", "8");
             file.close();
-            Log.e("Test", "9");
-            Log.e("Test", "10");
 
         } catch (Throwable e) {
             // Several error may come out with file handling or OOM
             e.printStackTrace();
             Log.e("photoSave", "CRASHED");
+        }
+
+    }
+    public void saveErrorLog(String path, String errorMessage){
+        try{
+        PrintWriter file;
+        File dir = new File(path);
+        dir.mkdir();
+        file = new PrintWriter(new FileOutputStream(new File(dir, (new SimpleDateFormat("MM-dd-yyyy-H:mm:ss").format(new Date())))));
+        file.println(errorMessage);
+        file.close();
+        }catch (IOException IOE){
+            Log.e("error log save", "failed");
         }
 
     }
